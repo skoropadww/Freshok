dbe10fc78477d277fe2a91673c3b39248370a8e9
Vitalii <skoropadww@gmail.com>
2021-08-02 17:54:20 +0300

    hw 13

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,17 @@ $(function(){
   $('.slider').slick({
     
   });
-  var mixer = mixitup('.goods__list');
+  let containerEl1 = document.querySelector('[data-ref="container-1"]');
+
+  let containerEl2 = document.querySelector('[data-ref="container-2"]');
+  
+  let config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  let mixer1 = mixitup(containerEl1, config);
+
+  let mixer2 = mixitup(containerEl2, config);
 })
