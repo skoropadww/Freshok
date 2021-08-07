9ac738ce7eb4d25c27dbd855ba2f06e23c44b865
Vitalii <skoropadww@gmail.com>
2021-08-07 22:02:33 +0300

    Очередные правки )) 07.08.2021

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,15 @@
 $(function(){
-  $('.hero__inner').slick({
-    
+  $('.hero__inner').slick({ 
   });
+
+  $('.brand__list').slick({
+    arrows: false,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+
   let containerEl1 = document.querySelector('[data-ref="container-1"]');
 
   let containerEl2 = document.querySelector('[data-ref="container-2"]');
@@ -15,4 +23,14 @@ $(function(){
   let mixer1 = mixitup(containerEl1, config);
 
   let mixer2 = mixitup(containerEl2, config);
+
+  $(".star__icon").rateYo({
+    starWidth: "16px",
+    ratedFill: "#FFB800" ,
+    rating: 5,
+    numStars: 1,
+    readOnly: true,
+    fullStar: true,
+  });
+
 })
--- gulpfile.js
+++ gulpfile.js
@@ -44,6 +44,7 @@ function scripts() {
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.min.js',
         'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/rateyo/src/jquery.rateyo.js',
         'app/js/main.js'
     ])
         .pipe(concat('main.min.js'))
